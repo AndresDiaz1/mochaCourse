172092681f9aaa0df90332a257fba6641285c06e
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-06 17:19:02 -0500

    added chai and expect

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,32 +1,15 @@
 var getPalette = require("../lib/getPalette");
 var assert = require("assert");
-var fs = require("fs");
-var configPath = process.cwd() + '/config.json'
-
-function writeConfig(config, callback){
-    fs.writeFile(configPath, JSON.stringify(config), callback);
-}
+var expect = require("chai").expect;
 
 describe('getPalette ', function(){
-
-    var config = {};
-
-    before(function(done){
-        fs.readFile(configPath, function(err, contents){
-            config = JSON.parse(contents.toString());
-            done();
-        });
-    });
-
-    afterEach(function (done){
-        writeConfig(config, done);
-    });
-
     it('should throw an error if palette is not an array', function(done){
-        writeConfig({ palette: "string"}, function(err){
-            assert.throws(getPalette , /Palette is not an array/);
-            done();
-        });
+        var notArray = function () {
+            getPalette(process.cwd() + '/test/fixtures/config-palette-non-array.json');
+        }
+        expect(notArray).to.throw (/Palette is not an array/);
+        done();
+
     });
 
     it('should return an array with 3 items by default', function() {
