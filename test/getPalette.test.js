ede57394a85145e5fa05a9908ccf1e24fa1cd22e
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-06 14:52:03 -0500

    a more complex unit tests suite

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -2,6 +2,10 @@ var getPalette = require("../lib/getPalette");
 var assert = require("assert");
 
 describe('getPalette ', function(){
+    it('should throw an error if palette is not an array', function(){
+        assert.throws(getPalette, /is not array/);
+    });
+
     it('should return an array with 3 items', function() {
         var palette = getPalette();
         assert(Array.isArray(palette));
