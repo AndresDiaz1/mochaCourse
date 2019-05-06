42a5ee1aefe0206d4874677daccb8cc0ff025e7e
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-06 16:46:39 -0500

    before and after hooks

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -1,14 +1,32 @@
 var getPalette = require("../lib/getPalette");
 var assert = require("assert");
+var fs = require("fs");
+var configPath = process.cwd() + '/config.json'
+
+function writeConfig(config, callback){
+    fs.writeFile(configPath, JSON.stringify(config), callback);
+}
 
 describe('getPalette ', function(){
-    it('should throw an error if palette is not an array', function(){
-        function fetch() {
-            return "not array";
-        }
-        assert.throws(function() {
-            getPalette(fetch);
-        }, /Palette is not an array/);
+
+    var config = {};
+
+    before(function(done){
+        fs.readFile(configPath, function(err, contents){
+            config = JSON.parse(contents.toString());
+            done();
+        });
+    });
+
+    afterEach(function (done){
+        writeConfig(config, done);
+    });
+
+    it('should throw an error if palette is not an array', function(done){
+        writeConfig({ palette: "string"}, function(err){
+            assert.throws(getPalette , /Palette is not an array/);
+            done();
+        });
     });
 
     it('should return an array with 3 items by default', function() {
