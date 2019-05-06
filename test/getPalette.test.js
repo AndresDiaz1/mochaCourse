8c2c8df2eb1ef5600f5dfa590b265dcf002492f9
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-06 15:53:21 -0500

    dependency injection

--- test/getPalette.test.js
+++ test/getPalette.test.js
@@ -3,10 +3,15 @@ var assert = require("assert");
 
 describe('getPalette ', function(){
     it('should throw an error if palette is not an array', function(){
-        assert.throws(getPalette, /is not array/);
+        function fetch() {
+            return "not array";
+        }
+        assert.throws(function() {
+            getPalette(fetch);
+        }, /Palette is not an array/);
     });
 
-    it('should return an array with 3 items', function() {
+    it('should return an array with 3 items by default', function() {
         var palette = getPalette();
         assert(Array.isArray(palette));
         assert.equal(palette.length,3);
