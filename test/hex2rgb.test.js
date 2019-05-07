3da80eec1ae8d634fb7b139ffee91ce39b6f4cae
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-05-07 14:09:12 -0500

    Added sinon and refactored hex2rgb

--- test/hex2rgb.test.js
+++ test/hex2rgb.test.js
@@ -1,17 +1,18 @@
 var hex2rgb = require("../lib/hex2rgb");
 var assert = require("assert");
+var sinon = require("sinon");
 
 describe('hex2rgb ', function(){
 
     it("should return an error if the value is not a hex code", function(done) {
-        hex2rgb("blue", function(error, result){
+        hex2rgb.convert("blue", function(error, result){
             assert(error);
             done();
         })
     });
 
     it('should return a correctly converted rgb value', function(done) {
-       hex2rgb("#ffffff", function(error, result){
+       hex2rgb.convert("#ffffff", function(error, result){
         assert.strictEqual(error, null);
         assert.deepEqual(result, [255, 255, 255]);
         done();
